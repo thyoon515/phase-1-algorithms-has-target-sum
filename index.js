939c3ed843ec8030a4e223bb4523e845ce45e107
thyoon515 <thyoon515@gmail.com>
2022-05-11 15:01:55 -0400

    commit tree ecc0076804ebe23e9e131104618effb15a179b2d

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    for(let k = i + 1; k < array.length; k++) {
+      if(array[i] + array[k] === target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n * n)
 */
 
 /* 
   Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
+  if sum of two variables in a array equals the target number then return true, if not false.
+  take one variable at a time and adding the second variable until finds the match for target number.
+  array[0] + array[1] === 10 
+  array[0] + array[2] === 10
+  ...
+  Iterate through array by each variables starting from [0] of an array and incrementing by 1 until it reaches the end of the array,
+  iterate through array starting from [1] until the end of an array, incrementing by 1
+  Sum the first and second line to check if it matches the target number,
+  if it matches return true
+  if not return false 
+  */
+ 
+ /*
+ Add written explanation of your solution here
+ iterate through each variables in the array 
+ then iterate throuhg each variables again but adding plus 1, because we are not adding the same variable twice.
+ add both iteration to see if it matches the target number if it does return true
+ if not return false
+
 */
 
 // You can run `node index.js` to view these console logs
